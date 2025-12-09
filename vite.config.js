8c145b2732ead168278e767d3023d2bb6fb22129
Nick Fettig <77179593+nickfettig@users.noreply.github.com>
2025-12-09 15:57:10 -0500

    dep update react-dom; killed react-scripts -> vite; scss deprecated BADLY

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  root: ".",
+  publicDir: "public",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+});
